c44d3ba440e4a2c92fe1f66644f8fea0c09df28d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-07-19 22:34:08 +0200

    Changed code generator implementation
    The code generator now makes use of a set of functions to create the different files that contain the counter code, giving a cleaner and easier to use interface.
    
    The segmented files (previously called "K files") generation no longer happens during the main counter file creation, meaning that the main counter file pointer no longer stays open pointlessly waiting for other files to finish writing.
    
    Cleaned up PUT macros.
    
    WIP: Implementation of a simple input arguments system to allow users to use the code generator as both a library (code) and a command line tool that can generate different counter files according to the user's chosen configuration and input arguments.

--- generator/generator2.c
+++ generator/generator2.c
@@ -1,76 +1,278 @@
 #include <stdio.h>
+#include <string.h>
 
-#define PUT(depth, FILE_POINTER, fmt, ...) do{ \
+/*
+	HELPER MACROS
+*/
+
+#define PUT_INTERNAL(depth, file, fmt, tab, end, ...) do{ \
 	for(int i = 0; i < depth; ++i) \
-		fprintf(FILE_POINTER, "\t"); \
-	fprintf(FILE_POINTER, fmt "\n", ##__VA_ARGS__); \
+		fprintf(file, tab); \
+	fprintf(file, fmt, ##__VA_ARGS__); \
+	fprintf(file, end); \
 }while(0)
 
-#define CREATE_K_FILE(x) do{\
-	int value = x; \
-	char buf[255] = {0}; \
-	snprintf(buf, 255, "my_counter_%d.h", x); \
-	FILE *fileitr = fopen(buf, "wb"); \
-	PUT(0, fileitr, "#if MY_COUNTER == %d", value - 1000); \
-		PUT(1, fileitr, "#undef MY_COUNTER"); \
-		PUT(1, fileitr, "#define MY_COUNTER %d", value - 1000 + 1); \
-	for(int i = 1; i < 1000; ++i) \
-	{ \
-		int value_itr = ((i) + (value) - (1000)); \
-		PUT(0, fileitr, "#elif MY_COUNTER == %d", value_itr); \
-			PUT(1, fileitr, "#undef MY_COUNTER"); \
-			PUT(1, fileitr, "#define MY_COUNTER %d", value_itr + 1); \
-	} \
-	PUT(0, fileitr, "#endif"); \
-	fclose(fileitr);\
-}while(0)
+#define PUT(depth, file, fmt, ...) PUT_INTERNAL(depth, file, fmt, "\t", "", ##__VA_ARGS__)
 
-#define MAKE_EMPTY_FILE(filename) do{ \
-	FILE *file = fopen(filename, "wb"); \
-	fprintf(file, "\n"); \
-	fclose(file); \
-}while(0)
+#define PUT_LN(depth, file, fmt, ...) PUT_INTERNAL(depth, file, fmt, "\t", "\n", ##__VA_ARGS__)
 
-int main()
+
+
+
+
+typedef struct {
+	char *text;
+	void (*function)(int argc, char **argv, int idx, int parameters);
+	int parameters;
+} argument_t;
+
+/*
+	It has been considered whether counter_min and counter_increment parameters should be added, but it has been decided against due to the fact that these are features that most users will not require, and those who do require it can implement them easily on their own.
+	
+	This generator is designed to give a basis for customizing user defined compiletime macro counters.
+	
+	For instance:
+		-Min value:
+			Call the counter macro and add a value to it. This value will be the min value.
+			This can be wrapped around yet another user defined macro, allowing for easier use.
+		
+		-Custom Increment:
+			Manually calculate the increment by multiplying or operating over the value returned by the macro. 
+*/
+
+#define MAKE_ARGS_FUNCTION(name, body) void name(int argc, char **argv, int idx, int parameters) body
+
+MAKE_ARGS_FUNCTION(fn_help,
 {
-	FILE *file;
+	fprintf(stdout, "--help : show this text.\n");
+	fprintf(stdout, "--counter_name : define a custom name for the generated counter.\n");
+	fprintf(stdout, "--counter_max : define a custom value for the max value the counter can count to.\n");
+	fprintf(stdout, "--iterations_per_file : define a custom number of iterations that will be stored on each generated file.\n");
+})
+
+MAKE_ARGS_FUNCTION(fn_set_counter_name, {
 	
-	file = fopen("my_counter.h", "wb");
+})
+
+MAKE_ARGS_FUNCTION(fn_set_counter_max, {
 	
-	PUT(0, file, "#ifndef MY_COUNTER");
-		PUT(1, file, "#ifdef __COUNTER__");
-			PUT(2, file, "#define MY_COUNTER __COUNTER__");
-			PUT(2, file, "#define UPDATE_MY_COUNTER() \"my_counter_empty.h\"");
-			PUT(2, file, "#define MY_COUNTER_TYPE \"__COUNTER__\"");
-		PUT(1, file, "#else");
-			PUT(2, file, "#define MY_COUNTER 0");
-			PUT(2, file, "#define UPDATE_MY_COUNTER() \"my_counter.h\"");
-			PUT(2, file, "#define MY_COUNTER_TYPE \"MY_COUNTER\"");
-		PUT(1, file, "#endif");
-		PUT(1, file, "#define MY_COUNTER_GET() \"my_counter_get.h\"");
-	PUT(0, file, "#else");
+})
+
+MAKE_ARGS_FUNCTION(fn_set_iterations_per_file, {
 	
-		PUT(1, file, "#if MY_COUNTER < 1000");
-			PUT(2, file, "#include \"my_counter_1000.h\"");
-			CREATE_K_FILE(1000);
-		for(int i = 2000; i < 10000; i+=1000)
+})
+
+static argument_t arguments[] = {
+	{"--help", fn_help, 0},
+	{"--counter_name", fn_set_counter_name, 1},
+	{"--counter_max", fn_set_counter_max, 1},
+	{"--iterations_per_file", fn_set_iterations_per_file, 1}
+};
+static size_t const arguments_length = sizeof(arguments) / sizeof(argument_t); /* ironically, a counter could be used on insertion within the array to actually count the elements lol. Not worth it imo, but a funny idea nonetheless... */
+
+int match_arg(int argc, char **argv, int idx)
+{
+	for(int i = 0; i < arguments_length; ++i)
+	{
+		if(!strcmp(arguments[i].text, argv[idx]))
 		{
-		PUT(1, file, "#elif MY_COUNTER < %d", i);
-			PUT(2, file, "#include \"my_counter_%d.h\"\n", i);
-			CREATE_K_FILE(i);
-			printf("Created k-file for counter up to %d\n", i);
+			arguments[i].function(argc, argv, idx, arguments[i].parameters);
+			return arguments[i].parameters;
 		}
-		PUT(1, file, "#endif");
+	}
+	printf("ERROR: Unknown argument...");
+	return -1;
+}
+
+int iterate_args(int argc, char **argv)
+{
+	for(int i = 1; i < argc; ++i)
+	{
+		int val = match_arg(argc, argv, i);
+		if(val < 0)
+			return 1;
+		i += val;
+	}
+	return 0;
+}
+
+/*
+	IDEAS : __COUNTER__ is global, so...
+	make it so that there's a parameter that allows you to define if you want to make a global counter
+	another that allows you to define if you want to allow the counter to use __COUNTER__ if available for your compiler (only happens if the counter is global)
+	change code gen to include custom counter names and user defined info.
 	
-	PUT(0, file, "#endif");
+	idea (OLD):
+	to create your own counter, make it one of 2 ways:
+
+	1) use the code generator with a custom name
+
+	2) modify the code generator to do the following:
+		have it so that within a file you include itself, allowing for a second preprocessor pass, and then use that to translate CAT2(MY_COUNTER, USER_DEFINED_NAME) to a proper token that can be used in a #define, allowing you to have code gen with macros, without having to re-generate anything through the code generation program.
+
+	MAYBE YOU HAVE TO CREATE YET ANOTHER CUSTOM LIBRARY, ONE FOR MAKING ARGS PARSING LESS OF A PAIN IN THE BUTT, LIKE WHAT YOU'RE DOING RIGHT NOW... OR YOU COULD JUST USE A LARGE SWITCH CASE...
+
+
+*/
+
+enum COUNTER_TYPE
+{
+	COUNTER_TYPE_GLOBAL = 0,
+	COUNTER_TYPE_UNIQUE
+};
+
+char const *counter_name = "MY_COUNTER_NEW";
+int counter_max = 10000;
+int counter_iterations_per_file = 1000;
+int counter_type = COUNTER_TYPE_UNIQUE;
+
+
+
+typedef struct {
+	char const *name;
+	int max_value;
+	int iterations_per_file;
+} counter_info;
+
+void generate_counter_main_file(counter_info *info) // generates the file my_counter.h
+{
+	char filename[255] = {0};
+	snprintf(filename, 255, "%s_counter.h", info->name);
+	
+	FILE *file;
+	file = fopen(filename, "wb");
+	
+	PUT_LN(0, file, "#define %s_USE_COMPILER_COUNTER %d", info->name, 0);
+	PUT_LN(0, file, "#ifndef %s", info->name);
+		PUT_LN(1, file, "#if (%s_USE_COMPILER_COUNTER) && defined(__COUNTER__)", info->name);
+			PUT_LN(2, file, "#define %s __COUNTER__", info->name);
+			PUT_LN(2, file, "#define %s_UPDATE() \"%s_counter_empty.h\"", info->name, info->name);
+			PUT_LN(2, file, "#define %s_TYPE \"__COUNTER__\"", info->name);
+		PUT_LN(1, file, "#else");
+			PUT_LN(2, file, "#define %s 0", info->name);
+			PUT_LN(2, file, "#define %s_UPDATE() \"%s_counter_update.h\"", info->name, info->name);
+			PUT_LN(2, file, "#define %s_TYPE \"DRA_COUNTER\"", info->name);
+		PUT_LN(1, file, "#endif");	
+	PUT_LN(0, file, "#endif");
 	
 	fclose(file);
+}
+
+void generate_counter_update_file(counter_info *info) // generates the file my_counter_update.h
+{
+	char filename[255] = {0};
+	snprintf(filename, 255, "%s_counter_update.h", info->name);
 	
-	MAKE_EMPTY_FILE("my_counter_empty.h");
+	FILE *file;
+	file = fopen(filename, "wb");
 	
-	file = fopen("my_counter_get.h", "wb");
-	fprintf(file, "MY_COUNTER;\n#include UPDATE_MY_COUNTER()\n");
+	PUT_LN(0, file, "#ifdef %s", info->name);
+	
+	int i = info->iterations_per_file;
+	
+	PUT_LN(1, file, "#if %s < %d", info->name, i);
+	PUT_LN(2, file, "#include \"%s_counter_%d.h\"", info->name, i);
+	
+	for(i = info->iterations_per_file * 2; i < info->max_value; i+=info->iterations_per_file)
+	{
+		PUT_LN(1, file, "#elif %s < %d", info->name, i);
+		PUT_LN(2, file, "#include \"%s_counter_%d.h\"", info->name, i);
+	}
+	
+	PUT_LN(1, file, "#elif %s <= %d", info->name, i);
+	PUT_LN(2, file, "#include \"%s_counter_%d.h\"", info->name, i);
+	
+	PUT_LN(1, file, "#endif");
+	
+	PUT_LN(0, file, "#endif");
+	
 	fclose(file);
+}
+
+void generate_counter_segment_file(counter_info *info, int segment) // generates the files my_counter_1000.h
+{
+	char filename[255] = {0};
+	snprintf(filename, 255, "%s_counter_%d.h", info->name, segment);
+	
+	FILE *file;
+	file = fopen(filename, "wb");
+	
+	int first_value = segment - info->iterations_per_file;
+	
+	PUT_LN(0, file, "#if %s == %d", info->name, first_value); // #if MY_COUNTER == 0
+	PUT_LN(1, file, "#undef %s", info->name);
+	PUT_LN(1, file, "#define %s %d", info->name,  first_value + 1);
+	
+	for(int i = first_value + 1; i < segment; ++i)
+	{
+		PUT_LN(0, file, "#elif %s == %d", info->name, i); // #if MY_COUNTER == 0
+		PUT_LN(1, file, "#undef %s", info->name);
+		PUT_LN(1, file, "#define %s %d", info->name,  i + 1);
+	}
+	
+	PUT_LN(0, file, "#endif");
+	
+	fclose(file);
+}
+
+void generate_counter_segment_files(counter_info *info)
+{
+	for(int i = info->iterations_per_file; i <= info->max_value; i+=info->iterations_per_file)
+		generate_counter_segment_file(info, i);
+}
+
+void generate_counter_empty_file(counter_info *info) // generates the file my_counter_empty.h
+{
+	char filename[255] = {0};
+	snprintf(filename, 255, "%s_counter_empty.h", info->name);
+	
+	FILE *file;
+	file = fopen(filename, "wb");
+	
+	fprintf(file, "\n");
+	
+	fclose(file);
+}
+
+void generate_counter_files(counter_info *info)
+{
+	generate_counter_main_file(info);
+	generate_counter_update_file(info);
+	generate_counter_empty_file(info);
+	generate_counter_segment_files(info);
+}
+
+
+/*
+	- counter file name
+	- counter macro name
+	- counter update macro name
+	- counter min value
+	- counter max value
+	- counter increment
+	- counter iterations per file
+
+*/
+
+/*
+	- counter name
+	- counter max
+	- counter iterations per file
+*/
+
+
+int main(int argc, char **argv) {
+	/*
+	int success = iterate_args(argc, argv);
+	if(success != 0)
+		return 1;
+	*/
+	
+	counter_info info;
+	info.name = "MY_COUNTER";
+	info.max_value = 10000;
+	info.iterations_per_file = 1000;
+	generate_counter_files(&info);
 	
 	return 0;
 }
